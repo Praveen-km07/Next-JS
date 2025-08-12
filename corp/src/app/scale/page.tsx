d02c2d7b92fbd9da3cc25f7c9978201dbb218352
Praveen-km07 <testingudemyprojects.gmail.com>
2025-08-12 14:00:08 +0200

    added corporate project files

---
+++ corp/src/app/scale/page.tsx
@@ -0,0 +1,8 @@
+import Hero from "@/components/hero"
+import scaleImg from "@/assets/images/scale.jpg"
+
+export default function ScalePage(){
+    return (
+        <Hero imgData={scaleImg} imgAlt="steel factory" title="Scale your app to infinity" />
+    )
+}
